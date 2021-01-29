51acb79f7a0c24bd170547cb9ab78b1ab0218635
leegeepum <77970942+leegeepum@users.noreply.github.com>
2021-01-29 17:21:52 +0800

    Add better solution to 1

--- strings.py
+++ strings.py
@@ -1,4 +1,6 @@
 """
+problem 1 
+
 Sample Input 1:
 3
 
@@ -13,3 +15,13 @@ for i in range(1, x * 2, 2):
     string = "#" * i
     string = string.center(1 + 2 * (x - 1))
     print(string)
+
+"""
+better solution
+"""
+ui = int(input())
+
+hash_lst = ["#" * i for i in range(1, ui * 2) if i % 2 != 0]
+
+for h in hash_lst:
+    print(h.center(len(hash_lst[-1])))
